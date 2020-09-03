52ac984db98e4896eb74a413af2156ac9057aa60
alexandre-roa <alexandre.z.roa@gmail.com>
2020-09-03 14:37:12 -0300

    rotes and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { View, StatusBar } from 'react-native';
 
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-  </>
+    <View style={{ backgroundColor: '#312e38' }} />
+    <Routes />
+  </NavigationContainer>
 );
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
+import logo from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logo} />
+      <Title>Faça seu Logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
